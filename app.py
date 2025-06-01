f8d29ff07b45877fb307ecd3a4d9883476b623b4
tws5d <tylersnead85@gmail.com>
2025-06-01 12:02:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -474,4 +474,4 @@ else:
 st.markdown("---")
 st.markdown("<div style='text-align: center; margin-top: -10px;'>☕ Coffee Helps Me Maintain This 🙂</div>", unsafe_allow_html=True)
 st.markdown("<div style='text-align: center;'><img src='https://raw.githubusercontent.com/tws5d/Cycle-Parlay-Evaluator/main/tipjar_qr.png' width='130'></div>", unsafe_allow_html=True)
-st.markdown("<div style='text-align: center; margin-top: -10px;'><a href='https://buymeacoffee.com/tws5d' target='_blank'>https://buymeacoffee.com/tws5d</a></div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; margin-top: 5px;'><a href='https://buymeacoffee.com/tws5d' target='_blank'>https://buymeacoffee.com/tws5d</a></div>", unsafe_allow_html=True)
