0c78e67a0219e11032508efaf2954a80fd1ea2ae
tws5d <tylersnead85@gmail.com>
2025-06-01 11:48:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -470,3 +470,7 @@ if not df.empty:
     
 else:
     st.warning("No Statcast data found for this timeframe.")
+    st.markdown("---")
+    st.markdown("<div style='text-align: center; margin-top: 40px;'>If this helped you, consider buying me a coffee ☕</div>", unsafe_allow_html=True)
+    st.image("tipjar_qr.png", width=130)
+    st.markdown("<div style='text-align: center; margin-top: -10px;'><a href='https://buymeacoffee.com/tws5d' target='_blank'>https://buymeacoffee.com/tws5d</a></div>", unsafe_allow_html=True)
